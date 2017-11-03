9c7467dc195d6508b07e46d1e5099fc2eeec3ab5
2014214128 <2402444249@qq.com>
2017-11-03 10:00:11 +0800

    create myTalker project

---
+++ src/com/zg/test/TestClient.java
@@ -0,0 +1,12 @@
+package com.zg.test;
+
+import com.zg.client.Client;
+
+/**
+ * Created by Administrator on 2017/11/2.
+ */
+public class TestClient {
+    public static void main(String[] args) {
+        new Client();
+    }
+}
---
+++ src/com/zg/test/TestServer.java
@@ -0,0 +1,12 @@
+package com.zg.test;
+
+import com.zg.server.Server;
+
+/**
+ * Created by Administrator on 2017/11/2.
+ */
+public class TestServer {
+    public static void main(String[] args) {
+        new Server();
+    }
+}
